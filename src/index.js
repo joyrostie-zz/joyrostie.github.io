197cbf68251aeeedc0d24e11685740e0650302f6
joyrostie <valentyn.dembitskyi@itera.no>
2019-01-28 12:01:41 +0200

    grooming

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App/App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/todoListReducer'
+import App from './components/App/App'
 
 const initialState = {
     currentItem: { text: '', key: 0, readonly: true },
